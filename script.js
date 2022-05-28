694c050fa2106f24afbe7e90f287e1c12512a8e6
Kozmin Yurii <koriolan2@gmail.com>
2022-05-28 12:28:10 +0300

    настроили асинхронное соединение с файлом

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let select = document.querySelector('#cars'),
+    output = document.querySelector('.output');
+
+select.addEventListener('change', ()=> {
+    const request = new XMLHttpRequest();
+   
+    request.addEventListener('readystatechange', (event) => {
+        
+        if (request.readyState === 4 && request.status === 200) {
+            
+            const data = JSON.parse(request.responseText);
+            
+            data.cars.forEach(item => {
+                if(item.brand === select.value) {
+                    output.innerHTML = 'Model: ' + item.model + '<br> Price: '+ item.price;
+                }
+            });
+        }
+    });
+    
+    request.open('GET', './cars.json');
+    
+    request.setRequestHeader('Content-type','application/json');
+    request.send();
+    
+    
+    console.dir(request); 
+});
+   
+    
