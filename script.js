2951b78f3b4a5e560de7a688d87e8ccf318b7e4c
Kozmin Yurii <koriolan2@gmail.com>
2022-05-28 14:10:29 +0300

    Пример отправки данных на сервер без перезагрузки всей страницы

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 'use strict';
 
-let select = document.querySelector('#cars'),
-    output = document.querySelector('.output');
+const sendForm = () => {
+    let form = document.querySelector('form'),
+        user = form.querySelector('[type=text]'),
+        email= form.querySelector('[type=mail]'),
+        mesDiv = document.querySelector('.message');
 
-select.addEventListener('change', ()=> {
-    const request = new XMLHttpRequest();
-   
-    request.addEventListener('readystatechange', (event) => {
-        
-        if (request.readyState === 4 && request.status === 200) {
-            
-            const data = JSON.parse(request.responseText);
-            
-            data.cars.forEach(item => {
-                if(item.brand === select.value) {
-                    output.innerHTML = 'Model: ' + item.model + '<br> Price: '+ item.price;
+    let messages = {
+        'success' : 'Ваші дані успішно відправлені',
+        'warning' : 'Дані віправлвються',
+        'error' : 'Помилка віправлення даних'
+    }
+
+    form.addEventListener('submit', (event)=> {
+        event.preventDefault();
+
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () =>{
+                mesDiv.classList.add('warning');
+                mesDiv.innerHTML = messages.warning;
+
+                if (request.readyState !== 4) {
+                    return;
                 }
-            });
-        }
-    });
-    
-    request.open('GET', './cars.json');
-    
-    request.setRequestHeader('Content-type','application/json');
-    request.send();
-});
+
+                if (request.status === 200) {
+                    mesDiv.classList.remove('warning');
+                    mesDiv.classList.add('succses')
+                    mesDiv.innerHTML = messages.success;
+
+                } else {
+                    mesDiv.classList.remove('warning');
+                    mesDiv.classList.add('error')
+                    mesDiv.innerHTML = messages.error;
+
+                }
+        });
+        request.open('POST', './send.php');
+        request.setRequestHeader('Content-type', 'application/json');
+        
+        const data = {};
+        data.user = user.value;
+        data.email = email.value;
+
+        const body = JSON.stringify(data);
+
+        request.send(body);
+        
+
+    })
+}
+
+sendForm();
+
    
     
